f192b7fe97375a465aed38436f29a06eba8e9431
vitalii boiko <vukelan@gmail.com>
2024-09-26 17:40:03 +0300

    static serve from data

--- src/App.ts
+++ src/App.ts
@@ -1,14 +1,13 @@
 import express from 'express';
 
-
 const app = express();
 
 app.use('/data', express.static('data'));
 
-app.get('/avatar/:avatarId', (req, res) => {
-  const { avatarId } = req.params;
-  console.log(`requested avatarId: ${avatarId}`);
-  res.sendStatus(200);
-});
+// app.get('/avatar/:avatarId', (req, res) => {
+//   const { avatarId } = req.params;
+//   console.log(`requested avatarId: ${avatarId}`);
+//   res.sendStatus(200);
+// });
 
 export default app;
