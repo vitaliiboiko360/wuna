136e0b17483203831dd643138c42fe03a78097b6
vitalii boiko <vukelan@gmail.com>
2024-09-26 17:46:39 +0300

    rename game server

--- startGameServer.sh
+++ startGameServer.sh
@@ -3,10 +3,10 @@ tsc
 for file in ./out/*.js; do
     # 
     # example of expression
-    # sed -i -E "s/(import.*PlayerWsConnection)/\1\.mjs/" WebSocketGameServer.mjs
+    # sed -i -E "s/(import.*PlayerWsConnection)/\1\.mjs/" WSGameServer.mjs
     #fileName = eval $(basename $file .mjs)
     #echo "${fileName}"
-    #if [ "${fileName}" = "WebSocketGameServer" ]; then
+    #if [ "${fileName}" = "WSGameServer" ]; then
       # echo "$file"
       # echo "echo s\/(import.*PlayerWsConnection)\/\1\.mjs\/" "$file"
       #echo ''
@@ -15,10 +15,10 @@ for file in ./out/*.js; do
     mv "$file" "./out/$(basename "$file" .js).mjs"
     sed -i -E "s/(import.*\/PlayerWsConnection)'/\1\.mjs'/" "./out/$(basename "$file" .js).mjs"
     sed -i -E "s/(import.*\/Game)'/\1\.mjs'/" "./out/$(basename "$file" .js).mjs"
-    sed -i -E "s/(import.*\/WebSocketGameServer)'/\1\.mjs'/" "./out/$(basename "$file" .js).mjs"
+    sed -i -E "s/(import.*\/WSGameServer)'/\1\.mjs'/" "./out/$(basename "$file" .js).mjs"
     sed -i -E "s/(import.*\/Cards)'/\1\.mjs'/" "./out/$(basename "$file" .js).mjs"
     sed -i -E "s/(import.*\/GameManager)'/\1\.mjs'/" "./out/$(basename "$file" .js).mjs"
     sed -i -E "s/(import.*\/ProcessMove)'/\1\.mjs'/" "./out/$(basename "$file" .js).mjs"
     sed -i -E "s/(import.*\/App)'/\1\.mjs'/" "./out/$(basename "$file" .js).mjs"
 done
-node ./out/WebSocketGameServer.mjs
+node ./out/WSGameServer.mjs
