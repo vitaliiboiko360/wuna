a016c21bff866491bacf97d3c4b72362d99ce5e6
vitalii boiko <vukelan@gmail.com>
2024-09-30 17:15:51 +0300

    up handle wildcard

--- cli/svg/svg_handlewildcard.tsx
+++ cli/svg/svg_handlewildcard.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import { useSvgContext } from './svg_container';
+
 export function HandleWildCard(props) {
 
-
+  const refSvg = useSvgContext();
 
   return (
-    <></>
+    <>
+      <g transform='translate(300,300)'>
+        <rect ></rect>
+      </g>
+    </>
   );
 }
