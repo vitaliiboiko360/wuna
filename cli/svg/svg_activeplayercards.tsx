cfd435115cd427e8f774119e594b3ebe261a0e39
vitaliiboiko360 <vukelan@gmail.com>
2024-10-03 22:56:40 +0300

    up handleWild; change formating

--- cli/svg/svg_activeplayercards.tsx
+++ cli/svg/svg_activeplayercards.tsx
@@ -1,6 +1,11 @@
-import React, { Fragment, useContext, useEffect } from 'react';
+import React, { Fragment, useContext, useEffect, useState } from 'react';
 
-import { getCard, isCardPlayable, isCardSameColor, getCardColor } from './svg_getcard';
+import {
+  getCard,
+  isCardPlayable,
+  isCardSameColor,
+  getCardColor,
+} from './svg_getcard';
 import getOnClickForCard from './active_players/getonlickforcards.ts';
 
 import { WebSocketContext } from '../websocketprovider.tsx';
@@ -8,6 +13,7 @@ import { useAppSelector, useAppDispatch } from '../store/hooks.ts';
 import { selectActivePlayerSeatNumber } from '../store/activePlayerSeatNumber.ts';
 import { selectActiveMoveWildCardColor } from '../store/activeMove.ts';
 import { USER_1 } from '../websocketconsumer.tsx';
+import { WILD } from '../../src/Cards.ts';
 
 function sendSkipMoveToServer(websocket, seatNumber) {
   let arrayToSend: Uint8Array = new Uint8Array(3);
@@ -20,13 +26,20 @@ function sendSkipMoveToServer(websocket, seatNumber) {
 export default function SvgActivePlayerCards(props) {
   const webSocket = useContext(WebSocketContext);
   const activePlayerSeatNumber = useAppSelector(selectActivePlayerSeatNumber);
-  const activeWildCardColorToPlay = useAppSelector(selectActiveMoveWildCardColor);
+  const activeWildCardColorToPlay = useAppSelector(
+    selectActiveMoveWildCardColor
+  );
 
   let playableCardCounter = 0;
 
   useEffect(() => {
     if (playableCardCounter == 0 && props.isOurTurn) {
-      console.log('props.isOurTurn =', props.isOurTurn, ' playableCardCounter=', playableCardCounter);
+      console.log(
+        'props.isOurTurn =',
+        props.isOurTurn,
+        ' playableCardCounter=',
+        playableCardCounter
+      );
     }
   }, []);
 
@@ -34,38 +47,69 @@ export default function SvgActivePlayerCards(props) {
 
   if (activePlayerSeatNumber == 0) {
     console.log(`activePlayerSeatNumber == 0`);
-    return (<></>);
+    return <></>;
   }
 
   //console.log(`props.cardsArray.length=${props.cardArray.length}`);
   let onClick: Function | undefined = undefined;
-  return (<>
-    {props
-      .cardArray
-      .map((card, index) => {
+  return (
+    <>
+      {(props.cardArray.length
+        ? props.cardArray.with(0, WILD.Wild)
+        : props.cardArray
+      ).map((card, index) => {
         let transformString = '';
-        const isPlayable = props.isOurTurn
-          && (isCardPlayable(card, props.activeCard) || isCardSameColor(card, activeWildCardColorToPlay));
+        const isPlayable =
+          props.isOurTurn &&
+          (isCardPlayable(card, props.activeCard) ||
+            isCardSameColor(card, activeWildCardColorToPlay));
         if (isPlayable) {
           playableCardCounter++;
-          transformString = `translate(${(index * 15) - 10},${-40})`;
+          transformString = `translate(${index * 15 - 10},${-40})`;
         } else
-          transformString = `translate(${(index * 15) - (10 * playableCardCounter)})`; // 20px -> step to the right on X axe
+          transformString = `translate(${
+            index * 15 - 10 * playableCardCounter
+          })`; // 20px -> step to the right on X axe
 
         if (typeof onClick === 'undefined' && props.isOurTurn && !isPlayable) {
-          onClick = () => setTimeout(() => sendSkipMoveToServer(webSocket, activePlayerSeatNumber), 1500);
+          onClick = () =>
+            setTimeout(
+              () => sendSkipMoveToServer(webSocket, activePlayerSeatNumber),
+              1500
+            );
         }
         if (props.isOurTurn && isCardSameColor(card, activeWildCardColorToPlay))
-          console.log('card=', card, ' activeWildCardColorToPlay=', activeWildCardColorToPlay,
-            'getCardColor(card)=', getCardColor(card),
-            ' isCardSameColor= ', isCardSameColor(card, activeWildCardColorToPlay));
+          console.log(
+            'card=',
+            card,
+            ' activeWildCardColorToPlay=',
+            activeWildCardColorToPlay,
+            'getCardColor(card)=',
+            getCardColor(card),
+            ' isCardSameColor= ',
+            isCardSameColor(card, activeWildCardColorToPlay)
+          );
         return (
           <Fragment key={index}>
-            <g transform={transformString} onClick={isPlayable ? getOnClickForCard(card, webSocket, activePlayerSeatNumber, dispatch, props.setPlayCardInfo) : onClick} >
+            <g
+              transform={transformString}
+              onClick={
+                isPlayable
+                  ? getOnClickForCard(
+                      card,
+                      webSocket,
+                      activePlayerSeatNumber,
+                      dispatch,
+                      props.setPlayCardInfo
+                    )
+                  : onClick
+              }
+            >
               {getCard(card)}
             </g>
           </Fragment>
         );
       })}
-  </>);
+    </>
+  );
 }
--- cli/svg/svg_handlewildcard.tsx
+++ cli/svg/svg_handlewildcard.tsx
@@ -1,50 +1,103 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+
+import { createPortal } from 'react-dom';
 
 import { useSvgContext } from './svg_container';
+import gsap from 'gsap';
 
 const DIMS = {
-  'width': 100,
-  'height': 100,
+  width: 100,
+  height: 100,
 };
 
+const COLORS = [{ fill: 'green' }];
+
 const LEFT_TOP_POSITIONS = {
-  'x': 0,
-  'y': 0,
-}
+  x: 0,
+  y: 0,
+};
 
 const LEFT_BOTTOM_POSITIONS = {
-  'x': 0,
-  'y': 120,
-}
+  x: 0,
+  y: 120,
+};
 
 const RIGHT_TOP_POSITIONS = {
-  'x': 120,
-  'y': 0,
-}
+  x: 120,
+  y: 0,
+};
 
 const RIGHT_BOTTOM_POSITIONS = {
-  'x': 120,
-  'y': 120,
-}
+  x: 120,
+  y: 120,
+};
 
 const RECT_ATTRIBUTES = {
-  fill: 'green',
   rx: 7,
   ry: 7,
 };
 
 export function HandleWildCard(props) {
-
   const refSvg = useSvgContext();
+
+  const refRect1 = useRef();
+  const refRect2 = useRef();
+  const refRect3 = useRef();
+  const refRect4 = useRef();
+
+  const refGroup = useRef();
+
+  useEffect(() => {
+    gsap.set(refGroup.current, { rotation: -180, transformOrigin: '50% 50%' });
+    gsap.to(refGroup.current, {
+      rotation: 180,
+      duration: 0.6,
+      transformOrigin: '50% 50%',
+    });
+    const animateObject = {
+      rotation: 360,
+      duration: 1.4,
+      transformOrigin: '50% 50%',
+    };
+    gsap.to(refRect1.current, animateObject);
+    gsap.to(refRect2.current, animateObject);
+    gsap.to(refRect3.current, animateObject);
+    gsap.to(refRect4.current, animateObject);
+  });
 
   return (
     <>
-      <g transform='translate(290,175)'>
-        <rect {...DIMS} {...LEFT_TOP_POSITIONS} {...RECT_ATTRIBUTES} />
-        <rect {...DIMS} {...LEFT_BOTTOM_POSITIONS} {...RECT_ATTRIBUTES} />
-        <rect {...DIMS} {...RIGHT_TOP_POSITIONS} {...RECT_ATTRIBUTES} />
-        <rect {...DIMS} {...RIGHT_BOTTOM_POSITIONS} {...RECT_ATTRIBUTES} />
-      </g>
+      {createPortal(
+        <>
+          <g ref={refGroup} transform='translate(290,175)'>
+            <rect
+              ref={refRect1}
+              {...DIMS}
+              {...LEFT_TOP_POSITIONS}
+              {...RECT_ATTRIBUTES}
+            />
+            <rect
+              ref={refRect2}
+              {...DIMS}
+              {...LEFT_BOTTOM_POSITIONS}
+              {...RECT_ATTRIBUTES}
+            />
+            <rect
+              ref={refRect3}
+              {...DIMS}
+              {...RIGHT_TOP_POSITIONS}
+              {...RECT_ATTRIBUTES}
+            />
+            <rect
+              ref={refRect4}
+              {...DIMS}
+              {...RIGHT_BOTTOM_POSITIONS}
+              {...RECT_ATTRIBUTES}
+            />
+          </g>
+        </>,
+        refSvg.current
+      )}
     </>
   );
 }
