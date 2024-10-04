9f61053c85ccf05114177882ad3aa2054624e196
vitaliiboiko360 <vukelan@gmail.com>
2024-10-04 15:15:54 +0300

    update color in wild card

--- cli/svg/svg_handlewildcard.tsx
+++ cli/svg/svg_handlewildcard.tsx
@@ -5,12 +5,22 @@ import { createPortal } from 'react-dom';
 import { useSvgContext } from './svg_container';
 import gsap from 'gsap';
 
-const COLORS = [
+const COLOR_ATTRIBUTES = [
   { fill: '#ff5555', stroke: '#d74a49' },
-  { fill: '#5555ff', stroke: '#4b49d6' },
-  { fill: '#ffaa00', stroke: '#d7900e' },
   { fill: '#00aa00', stroke: '#12900b' },
+  { fill: '#ffaa00', stroke: '#d7900e' },
+  { fill: '#5555ff', stroke: '#4b49d6' },
 ];
+
+const enum COLOR_INDEX {
+  RED = 0,
+  GREEN,
+  BLUE,
+  YELLOW,
+}
+const COLORS = ['#ff5555', '#00aa00', '#ffaa00', '#5555ff'];
+
+type ElementType = Element | SVGElement | null | undefined;
 
 const GROUP_POSITION = {
   x: 305,
@@ -20,25 +30,25 @@ const GROUP_POSITION = {
 const LEFT_TOP_ATTRIBUTES = {
   x: 0,
   y: 0,
-  ...COLORS[0],
+  ...COLOR_ATTRIBUTES[0],
 };
 
 const LEFT_BOTTOM_ATTRIBUTES = {
   x: 0,
   y: 105,
-  ...COLORS[3],
+  ...COLOR_ATTRIBUTES[3],
 };
 
 const RIGHT_TOP_ATTRIBUTES = {
   x: 105,
   y: 0,
-  ...COLORS[1],
+  ...COLOR_ATTRIBUTES[1],
 };
 
 const RIGHT_BOTTOM_ATTRIBUTES = {
   x: 105,
   y: 105,
-  ...COLORS[2],
+  ...COLOR_ATTRIBUTES[2],
 };
 
 const RECT_ATTRIBUTES = {
@@ -79,34 +89,48 @@ export function HandleWildCard(props) {
     gsap.to(refRect4.current, animateObject);
   });
 
-  let onClick = (event) => {
-    [refRect1, refRect2, refRect3, refRect4].forEach((ref) => {
-      if (!ref.current.isSameNode(event.currentTarget)) {
-        ref.current.remove();
-      }
-    });
-    const wildCard = refGroup.current.previousElementSibling;
-    console.log(`wildCard=${wildCard}`);
-    console.log(`wildCard=${wildCard.children}`);
-    if (!wildCard) {
-      console.log(`wildCard=${wildCard}`);
-      return;
-    }
-
-    let currentElement = wildCard.children.item(0);
-    let traverseArray: Element[] = [];
+  const updateColorOnWildCard = function (
+    wildCardElement: SVGElement,
+    hexColorToChangeTo: string
+  ) {
+    console.log(this);
+    let currentElement: ElementType = wildCardElement.children.item(0);
+    let traverseArray: ElementType[] = [];
     while (currentElement) {
       if (currentElement.children.length) {
         traverseArray.push(currentElement.children.item(0));
       }
       if (
-        currentElement.tagName == 'path' ||
-        currentElement.tagName == 'PATH'
+        currentElement.tagName == 'rect' ||
+        currentElement.tagName == 'RECT'
       ) {
-        console.log(currentElement.style.fill);
+        if (
+          currentElement.style.fill == '#000000' ||
+          currentElement.style.fill == 'rgb(0, 0, 0)'
+        ) {
+          currentElement.style.fill = hexColorToChangeTo;
+        }
+        console.log(currentElement);
       }
+
       currentElement =
         currentElement.nextElementSibling ?? traverseArray.shift();
+    }
+  };
+
+  const onClick = (event) => {
+    console.log(this);
+    let colorToChangeTo = '';
+    [refRect1, refRect2, refRect3, refRect4].forEach((ref) => {
+      if (!ref.current.isSameNode(event.currentTarget)) {
+        ref.current.remove();
+      } else {
+        colorToChangeTo = ref.current.fill;
+      }
+    });
+    const wildCard = refGroup.current.previousElementSibling;
+    if (wildCard) {
+      updateColorOnWildCard(wildCard, colorToChangeTo);
     }
   };
 
