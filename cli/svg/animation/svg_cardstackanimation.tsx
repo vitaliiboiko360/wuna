5404c27fb306a2fda52e8e1c0dd50899c4304e41
vitaliiboiko360 <vukelan@gmail.com>
2024-10-06 21:25:45 +0300

    add selectors to animation

--- cli/svg/animation/svg_cardstackanimation.tsx
+++ cli/svg/animation/svg_cardstackanimation.tsx
@@ -1,12 +1,31 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { getBlankBacksideCard } from '../svg_getcard.tsx';
+import { useAppSelector } from '../../store/hooks.ts';
+import {
+  selectActiveMoveLastPlayerCard,
+  selectActiveMoveLastPlayer,
+} from '../../store/activeMove.ts';
+
 export function SvgCardStackAnimation(props) {
   const refTopCard = useRef();
+  const lastPlayerCardId = useAppSelector(selectActiveMoveLastPlayerCard);
+  const lastPlayerId = useAppSelector(selectActiveMoveLastPlayer);
+
+  let pathToDraw;
+  if (!lastPlayerCardId && lastPlayerId) {
+    pathToDraw = (
+      <>
+        <path d='' fill='none' stroke='red' />
+      </>
+    );
+  }
+
   return (
     <>
       <g ref={refTopCard} transform={`matrix(0.5,0.8,-0.905,0.2,280,130)`}>
         {getBlankBacksideCard()}
       </g>
+      {pathToDraw}
     </>
   );
 }
