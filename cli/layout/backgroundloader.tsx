b427c7421ec2f4173deb5a59cf12ef907df63641
vitalii boiko <vukelan@gmail.com>
2024-10-17 18:55:57 +0300

    experimenting videos

--- cli/layout/backgroundloader.tsx
+++ cli/layout/backgroundloader.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import * as css from './layout.scss';
+
+const paths = [
+  'data/bg/172144-846731218_large.mp4',
+  'data/bg/180075-863401737_moon_house.mp4',
+  'data/bg/201290-915375225_milton_resort.mp4',
+];
 export function BackgroundLoader(props) {
   return (
     <>
       <div className={css.flexVideoContainer}>
         <video autoPlay loop muted>
-          <source
-            src="data/bg/180075-863401737_moon_house.mp4"
-            type="video/mp4"
-          ></source>
+          <source src={paths[2]} type="video/mp4"></source>
         </video>
       </div>
     </>
