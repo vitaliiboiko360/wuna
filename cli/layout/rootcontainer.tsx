62fe9d5817ba8a3235fd9aaa092bed232c0fa418
vitalii boiko <vukelan@gmail.com>
2024-10-17 16:30:35 +0300

    add conditional styling to div svg

--- cli/layout/rootcontainer.tsx
+++ cli/layout/rootcontainer.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
-
+import React, { useEffect, useRef } from 'react';
+import { selectActivePlayerSeatNumber } from '../store/activePlayerSeatNumber.ts';
+import { useAppSelector } from '../store/hooks.ts';
 import * as css from './rootcontainer.scss';
 
 export function RootContainer(props) {
-  return <div className={css.rootContainer}>{props.children}</div>;
+  const activePlayerSeatNumber = useAppSelector(selectActivePlayerSeatNumber);
+  const refDivSvg = useRef();
+  useEffect(() => {
+    if (activePlayerSeatNumber != 0) {
+      refDivSvg.current?.classList.replace(
+        css.svgContainerEmpty,
+        css.svgContainer
+      );
+    }
+  }, [activePlayerSeatNumber]);
+
+  return (
+    <>
+      <div className={css.rootContainer}>
+        <div className={css.bgContainer}>{props.children[0]}</div>
+        <div ref={refDivSvg} className={css.svgContainerEmpty}>
+          {props.children[1]}
+        </div>
+      </div>
+    </>
+  );
 }
