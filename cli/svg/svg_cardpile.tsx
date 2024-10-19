e042a4aacae709cecdfe0112df7dcfd38f657ac7
vitalii boiko <vukelan@gmail.com>
2024-10-19 22:29:56 +0300

    rename to attributes

--- cli/svg/svg_cardpile.tsx
+++ cli/svg/svg_cardpile.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Card from './svg_card';
-import { SVG_DIMENSIONS } from './svg_container';
+import { SVG_ATTRIBUTES } from './svg_container';
 
-export const xCenter = SVG_DIMENSIONS.width / 2;
-export const yCenter = SVG_DIMENSIONS.height / 2;
+export const xCenter = SVG_ATTRIBUTES.width / 2;
+export const yCenter = SVG_ATTRIBUTES.height / 2;
 
 export default function SvgCardPile(props) {
   return (
@@ -36,33 +36,33 @@ export function SvgCardPaths() {
     <>
       <path
         id={IDPATH[0]}
-        d='M120,300 C120,300 300,260 400,300'
-        fill='none'
+        d="M120,300 C120,300 300,260 400,300"
+        fill="none"
       ></path>
       <path
         id={IDPATH[1]}
-        d='M400,170 C400,170 380,230 400,300'
-        fill='none'
+        d="M400,170 C400,170 380,230 400,300"
+        fill="none"
       ></path>
       <path
         id={IDPATH[2]}
-        d='M400,170 C400,170 420,230 400,300'
-        fill='none'
+        d="M400,170 C400,170 420,230 400,300"
+        fill="none"
       ></path>
       <path
         id={IDPATH[3]}
-        d='M680,300 C680,300 530,260 400,300'
-        fill='none'
+        d="M680,300 C680,300 530,260 400,300"
+        fill="none"
       ></path>
       <path
         id={IDPATH[4]}
-        d='M400,430 C400,430 420,360 400,300'
-        fill='none'
+        d="M400,430 C400,430 420,360 400,300"
+        fill="none"
       ></path>
       <path
         id={IDPATH[5]}
-        d='M400,430 C400,430 380,360 400,300'
-        fill='none'
+        d="M400,430 C400,430 380,360 400,300"
+        fill="none"
       ></path>
     </>
   );
