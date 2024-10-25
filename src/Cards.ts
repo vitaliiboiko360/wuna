309ea4ac225a144c8778855d8df8613d6451c683
vitalii boiko <vukelan@gmail.com>
2024-10-25 15:29:35 +0300

    formatting

--- src/Cards.ts
+++ src/Cards.ts
@@ -1,4 +1,3 @@
-
 export const NUBMER_OF_CARDS = 54;
 export const NUBMER_OF_DECKS = 4;
 export const NUMBER_OF_COLOR_CARDS = 13;
@@ -9,13 +8,13 @@ export const enum COLOR {
   RED,
   GREEN,
   BLUE,
-  YELLOW
+  YELLOW,
 }
 
 // wild cards
 export enum WILD {
   Wild = 14,
-  Draw4
+  Draw4,
 }
 
 // red, green, blue, yelow
@@ -32,7 +31,7 @@ export enum RED {
   _9,
   _Reverse,
   _Skip,
-  _Draw2
+  _Draw2,
 }
 
 export enum GREEN {
@@ -48,7 +47,7 @@ export enum GREEN {
   _9,
   _Reverse,
   _Skip,
-  _Draw2
+  _Draw2,
 }
 
 export enum BLUE {
@@ -64,7 +63,7 @@ export enum BLUE {
   _9,
   _Reverse,
   _Skip,
-  _Draw2
+  _Draw2,
 }
 
 export enum YELLOW {
@@ -80,42 +79,28 @@ export enum YELLOW {
   _9,
   _Reverse,
   _Skip,
-  _Draw2
+  _Draw2,
 }
 
 export function isValidCard(idOfCard: number) {
-  if (idOfCard == WILD.Draw4
-    || idOfCard == WILD.Wild)
-    return true;
-  if (idOfCard >= RED._0
-    && idOfCard <= RED._Draw2)
-    return true;
-  if (idOfCard >= GREEN._0
-    && idOfCard <= GREEN._Draw2)
-    return true;
-  if (idOfCard >= BLUE._0
-    && idOfCard <= BLUE._Draw2)
-    return true;
-  if (idOfCard >= YELLOW._0
-    && idOfCard <= YELLOW._Draw2)
-    return true;
+  if (idOfCard == WILD.Draw4 || idOfCard == WILD.Wild) return true;
+  if (idOfCard >= RED._0 && idOfCard <= RED._Draw2) return true;
+  if (idOfCard >= GREEN._0 && idOfCard <= GREEN._Draw2) return true;
+  if (idOfCard >= BLUE._0 && idOfCard <= BLUE._Draw2) return true;
+  if (idOfCard >= YELLOW._0 && idOfCard <= YELLOW._Draw2) return true;
 
   return false;
 }
 
 export function isWildCard(idOfCard: number) {
-  return (idOfCard == WILD.Draw4 || idOfCard == WILD.Wild);
+  return idOfCard == WILD.Draw4 || idOfCard == WILD.Wild;
 }
 
 export function getCardColor(idOfCard: number) {
-  if (idOfCard & 0b01000000)
-    return 4;
-  if ((idOfCard & 0b00100000) && (idOfCard & 0b00010000))
-    return 3;
-  if (idOfCard & 0b00100000)
-    return 2;
-  if (idOfCard & 0b00010000)
-    return 1;
+  if (idOfCard & 0b01000000) return 4;
+  if (idOfCard & 0b00100000 && idOfCard & 0b00010000) return 3;
+  if (idOfCard & 0b00100000) return 2;
+  if (idOfCard & 0b00010000) return 1;
 
   return 0;
 }
@@ -143,10 +128,12 @@ export function getCardValue(idOfCard: number) {
 }
 
 export function isValidStartCard(idOfCard: number) {
-  return ((idOfCard >= RED._0 && idOfCard <= RED._9) ||
+  return (
+    (idOfCard >= RED._0 && idOfCard <= RED._9) ||
     (idOfCard >= GREEN._0 && idOfCard <= GREEN._9) ||
     (idOfCard >= BLUE._0 && idOfCard <= BLUE._9) ||
-    (idOfCard >= YELLOW._0 && idOfCard <= YELLOW._9));
+    (idOfCard >= YELLOW._0 && idOfCard <= YELLOW._9)
+  );
 }
 
 export function isReverseCard(idOfCard: number) {
@@ -166,7 +153,8 @@ export function isReverseCard(idOfCard: number) {
 }
 
 export function isSkipOrDrawCard(idOfCard: number) {
-  if (idOfCard == RED._Draw2 ||
+  if (
+    idOfCard == RED._Draw2 ||
     idOfCard == RED._Skip ||
     idOfCard == GREEN._Draw2 ||
     idOfCard == GREEN._Skip ||
@@ -182,7 +170,8 @@ export function isSkipOrDrawCard(idOfCard: number) {
 }
 
 export function isSkipCard(idOfCard: number) {
-  if (idOfCard == RED._Skip ||
+  if (
+    idOfCard == RED._Skip ||
     idOfCard == GREEN._Skip ||
     idOfCard == BLUE._Skip ||
     idOfCard == YELLOW._Skip
@@ -193,145 +182,193 @@ export function isSkipCard(idOfCard: number) {
 }
 
 export function isCardPlayable(idOfCard: number, topCard: number) {
-  if (idOfCard == WILD.Wild ||
-    idOfCard == WILD.Wild
+  if (idOfCard == WILD.Wild || idOfCard == WILD.Wild) {
+    return true;
+  }
+  if (
+    (idOfCard == RED._Draw2 ||
+      idOfCard == GREEN._Draw2 ||
+      idOfCard == BLUE._Draw2 ||
+      idOfCard == YELLOW._Draw2) &&
+    (topCard == RED._Draw2 ||
+      topCard == GREEN._Draw2 ||
+      topCard == BLUE._Draw2 ||
+      topCard == YELLOW._Draw2)
   ) {
     return true;
   }
-  if ((idOfCard == RED._Draw2 ||
-    idOfCard == GREEN._Draw2 ||
-    idOfCard == BLUE._Draw2 ||
-    idOfCard == YELLOW._Draw2
-  ) && (topCard == RED._Draw2 ||
-    topCard == GREEN._Draw2 ||
-    topCard == BLUE._Draw2 ||
-    topCard == YELLOW._Draw2)) {
+  if (
+    (idOfCard == RED._Reverse ||
+      idOfCard == GREEN._Reverse ||
+      idOfCard == BLUE._Reverse ||
+      idOfCard == YELLOW._Reverse) &&
+    (topCard == RED._Reverse ||
+      topCard == GREEN._Reverse ||
+      topCard == BLUE._Reverse ||
+      topCard == YELLOW._Reverse)
+  ) {
     return true;
   }
-  if ((idOfCard == RED._Reverse ||
-    idOfCard == GREEN._Reverse ||
-    idOfCard == BLUE._Reverse ||
-    idOfCard == YELLOW._Reverse
-  ) && (topCard == RED._Reverse ||
-    topCard == GREEN._Reverse ||
-    topCard == BLUE._Reverse ||
-    topCard == YELLOW._Reverse)) {
+  if (
+    (idOfCard == RED._Skip ||
+      idOfCard == GREEN._Skip ||
+      idOfCard == BLUE._Skip ||
+      idOfCard == YELLOW._Skip) &&
+    (topCard == RED._Skip ||
+      topCard == GREEN._Skip ||
+      topCard == BLUE._Skip ||
+      topCard == YELLOW._Skip)
+  ) {
     return true;
   }
-  if ((idOfCard == RED._Skip ||
-    idOfCard == GREEN._Skip ||
-    idOfCard == BLUE._Skip ||
-    idOfCard == YELLOW._Skip
-  ) && (topCard == RED._Skip ||
-    topCard == GREEN._Skip ||
-    topCard == BLUE._Skip ||
-    topCard == YELLOW._Skip)) {
+  if (
+    (idOfCard == RED._0 ||
+      idOfCard == GREEN._0 ||
+      idOfCard == BLUE._0 ||
+      idOfCard == YELLOW._0) &&
+    (topCard == RED._0 ||
+      topCard == GREEN._0 ||
+      topCard == BLUE._0 ||
+      topCard == YELLOW._0)
+  ) {
     return true;
   }
-  if ((idOfCard == RED._0 ||
-    idOfCard == GREEN._0 ||
-    idOfCard == BLUE._0 ||
-    idOfCard == YELLOW._0
-  ) && (topCard == RED._0 ||
-    topCard == GREEN._0 ||
-    topCard == BLUE._0 ||
-    topCard == YELLOW._0)) {
+  if (
+    (idOfCard == RED._1 ||
+      idOfCard == GREEN._1 ||
+      idOfCard == BLUE._1 ||
+      idOfCard == YELLOW._1) &&
+    (topCard == RED._1 ||
+      topCard == GREEN._1 ||
+      topCard == BLUE._1 ||
+      topCard == YELLOW._1)
+  ) {
     return true;
   }
-  if ((idOfCard == RED._1 ||
-    idOfCard == GREEN._1 ||
-    idOfCard == BLUE._1 ||
-    idOfCard == YELLOW._1
-  ) && (topCard == RED._1 ||
-    topCard == GREEN._1 ||
-    topCard == BLUE._1 ||
-    topCard == YELLOW._1)) {
+  if (
+    (idOfCard == RED._2 ||
+      idOfCard == GREEN._2 ||
+      idOfCard == BLUE._2 ||
+      idOfCard == YELLOW._2) &&
+    (topCard == RED._2 ||
+      topCard == GREEN._2 ||
+      topCard == BLUE._2 ||
+      topCard == YELLOW._2)
+  ) {
     return true;
   }
-  if ((idOfCard == RED._2 ||
-    idOfCard == GREEN._2 ||
-    idOfCard == BLUE._2 ||
-    idOfCard == YELLOW._2
-  ) && (topCard == RED._2 ||
-    topCard == GREEN._2 ||
-    topCard == BLUE._2 ||
-    topCard == YELLOW._2)) {
+  if (
+    (idOfCard == RED._3 ||
+      idOfCard == GREEN._3 ||
+      idOfCard == BLUE._3 ||
+      idOfCard == YELLOW._3) &&
+    (topCard == RED._3 ||
+      topCard == GREEN._3 ||
+      topCard == BLUE._3 ||
+      topCard == YELLOW._3)
+  ) {
     return true;
   }
-  if ((idOfCard == RED._3 ||
-    idOfCard == GREEN._3 ||
-    idOfCard == BLUE._3 ||
-    idOfCard == YELLOW._3
-  ) && (topCard == RED._3 ||
-    topCard == GREEN._3 ||
-    topCard == BLUE._3 ||
-    topCard == YELLOW._3)) {
+  if (
+    (idOfCard == RED._4 ||
+      idOfCard == GREEN._4 ||
+      idOfCard == BLUE._4 ||
+      idOfCard == YELLOW._4) &&
+    (topCard == RED._4 ||
+      topCard == GREEN._4 ||
+      topCard == BLUE._4 ||
+      topCard == YELLOW._4)
+  ) {
     return true;
   }
-  if ((idOfCard == RED._4 ||
-    idOfCard == GREEN._4 ||
-    idOfCard == BLUE._4 ||
-    idOfCard == YELLOW._4
-  ) && (topCard == RED._4 ||
-    topCard == GREEN._4 ||
-    topCard == BLUE._4 ||
-    topCard == YELLOW._4)) {
+  if (
+    (idOfCard == RED._5 ||
+      idOfCard == GREEN._5 ||
+      idOfCard == BLUE._5 ||
+      idOfCard == YELLOW._5) &&
+    (topCard == RED._5 ||
+      topCard == GREEN._5 ||
+      topCard == BLUE._5 ||
+      topCard == YELLOW._5)
+  ) {
     return true;
   }
-  if ((idOfCard == RED._5 ||
-    idOfCard == GREEN._5 ||
-    idOfCard == BLUE._5 ||
-    idOfCard == YELLOW._5
-  ) && (topCard == RED._5 ||
-    topCard == GREEN._5 ||
-    topCard == BLUE._5 ||
-    topCard == YELLOW._5)) {
+  if (
+    (idOfCard == RED._6 ||
+      idOfCard == GREEN._6 ||
+      idOfCard == BLUE._6 ||
+      idOfCard == YELLOW._6) &&
+    (topCard == RED._6 ||
+      topCard == GREEN._6 ||
+      topCard == BLUE._6 ||
+      topCard == YELLOW._6)
+  ) {
     return true;
   }
-  if ((idOfCard == RED._6 ||
-    idOfCard == GREEN._6 ||
-    idOfCard == BLUE._6 ||
-    idOfCard == YELLOW._6
-  ) && (topCard == RED._6 ||
-    topCard == GREEN._6 ||
-    topCard == BLUE._6 ||
-    topCard == YELLOW._6)) {
+  if (
+    (idOfCard == RED._7 ||
+      idOfCard == GREEN._7 ||
+      idOfCard == BLUE._7 ||
+      idOfCard == YELLOW._7) &&
+    (topCard == RED._7 ||
+      topCard == GREEN._7 ||
+      topCard == BLUE._7 ||
+      topCard == YELLOW._7)
+  ) {
     return true;
   }
-  if ((idOfCard == RED._7 ||
-    idOfCard == GREEN._7 ||
-    idOfCard == BLUE._7 ||
-    idOfCard == YELLOW._7
-  ) && (topCard == RED._7 ||
-    topCard == GREEN._7 ||
-    topCard == BLUE._7 ||
-    topCard == YELLOW._7)) {
+  if (
+    (idOfCard == RED._8 ||
+      idOfCard == GREEN._8 ||
+      idOfCard == BLUE._8 ||
+      idOfCard == YELLOW._8) &&
+    (topCard == RED._8 ||
+      topCard == GREEN._8 ||
+      topCard == BLUE._8 ||
+      topCard == YELLOW._8)
+  ) {
     return true;
   }
-  if ((idOfCard == RED._8 ||
-    idOfCard == GREEN._8 ||
-    idOfCard == BLUE._8 ||
-    idOfCard == YELLOW._8
-  ) && (topCard == RED._8 ||
-    topCard == GREEN._8 ||
-    topCard == BLUE._8 ||
-    topCard == YELLOW._8)) {
+  if (
+    (idOfCard == RED._9 ||
+      idOfCard == GREEN._9 ||
+      idOfCard == BLUE._9 ||
+      idOfCard == YELLOW._9) &&
+    (topCard == RED._9 ||
+      topCard == GREEN._9 ||
+      topCard == BLUE._9 ||
+      topCard == YELLOW._9)
+  ) {
     return true;
   }
-  if ((idOfCard == RED._9 ||
-    idOfCard == GREEN._9 ||
-    idOfCard == BLUE._9 ||
-    idOfCard == YELLOW._9
-  ) && (topCard == RED._9 ||
-    topCard == GREEN._9 ||
-    topCard == BLUE._9 ||
-    topCard == YELLOW._9)) {
+  if (
+    idOfCard >= RED._0 &&
+    idOfCard <= RED._Draw2 &&
+    topCard >= RED._0 &&
+    topCard <= RED._Draw2
+  )
     return true;
-  }
-  if ((idOfCard >= RED._0 && idOfCard <= RED._Draw2) && (topCard >= RED._0 && topCard <= RED._Draw2)) return true;
-  if ((idOfCard >= GREEN._0 && idOfCard <= GREEN._Draw2) && (topCard >= GREEN._0 && topCard <= GREEN._Draw2)) return true;
-  if ((idOfCard >= BLUE._0 && idOfCard <= BLUE._Draw2) && (topCard >= BLUE._0 && topCard <= BLUE._Draw2)) return true;
-  if ((idOfCard >= YELLOW._0 && idOfCard <= YELLOW._Draw2) && (topCard >= YELLOW._0 && topCard <= YELLOW._Draw2)) return true;
+  if (
+    idOfCard >= GREEN._0 &&
+    idOfCard <= GREEN._Draw2 &&
+    topCard >= GREEN._0 &&
+    topCard <= GREEN._Draw2
+  )
+    return true;
+  if (
+    idOfCard >= BLUE._0 &&
+    idOfCard <= BLUE._Draw2 &&
+    topCard >= BLUE._0 &&
+    topCard <= BLUE._Draw2
+  )
+    return true;
+  if (
+    idOfCard >= YELLOW._0 &&
+    idOfCard <= YELLOW._Draw2 &&
+    topCard >= YELLOW._0 &&
+    topCard <= YELLOW._Draw2
+  )
+    return true;
 
   return false;
 }
