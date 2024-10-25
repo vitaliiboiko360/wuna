f1f13591b16e7ebb4e3dd7d07ef0d8b75afc7fcd
vitalii boiko <vukelan@gmail.com>
2024-10-25 19:13:10 +0300

    output game results

--- cli/gameresults/endgameresultschecker.tsx
+++ cli/gameresults/endgameresultschecker.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
 import { useAppSelector } from '../store/hooks.ts';
-
+import { selectGameResults } from '../store/gameResults.ts';
 import EndGameResultsOutput from './gameresultsoutput.tsx';
 
 export default function EndGameResultsChecker(props) {
   // console.log(`\t:::\tprops.svgGameField= ${props.svgGameField}`);
-
-  return <>{/* <EndGameResultsOutput /> */}</>;
+  const gameResults = useAppSelector(selectGameResults);
+  return (
+    <>
+      {gameResults.isReady && (
+        <EndGameResultsOutput gameResults={gameResults} />
+      )}
+    </>
+  );
 }
--- cli/gameresults/gameresultsoutput.tsx
+++ cli/gameresults/gameresultsoutput.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import * as css from './endgameresults.scss';
+import * as css from './gameresults.scss';
 
 import { gsap } from 'gsap';
 
 export default function EndGameResultsOutput(props) {
+  const { gameResults } = props;
+  const { firstPlace, secondPlace, thirdPlace, fourthPlace } = gameResults;
   return (
     <div className={css.outerDiv}>
       <div className={css.innerDiv}>
-        <div className={'player-line'}></div>
-        <div className={'player-line'}></div>
-        <div className={'player-line'}></div>
-        <div className={'player-line'}></div>
+        <div className={'player-line'}>{firstPlace.join(' ')}</div>
+        <div className={'player-line'}>{secondPlace.join(' ')}</div>
+        <div className={'player-line'}>{thirdPlace.join(' ')}</div>
+        <div className={'player-line'}>{fourthPlace.join(' ')}</div>
       </div>
     </div>
   );
