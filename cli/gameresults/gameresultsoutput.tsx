1613df00744c060c8a69490e149633e1215d36a5
vitalii boiko <vukelan@gmail.com>
2024-10-28 20:27:26 +0200

    add player score line to game results

--- cli/gameresults/gameresultsoutput.tsx
+++ cli/gameresults/gameresultsoutput.tsx
@@ -4,6 +4,7 @@ import * as css from './gameresults.scss';
 import SvgGameResultsContainer from './svg_gameresults_container';
 
 import { gsap } from 'gsap';
+import SvgPlayerScoreLine, { PLACE } from './svg_gameresults_playerscoreline';
 
 export default function EndGameResultsOutput(props) {
   const { gameResults } = props;
@@ -12,10 +13,36 @@ export default function EndGameResultsOutput(props) {
     <div className={css.outerDiv}>
       <div className={css.innerDiv}>
         <SvgGameResultsContainer>
-          <div className={'player-line'}>{firstPlace.join(' ')}</div>
-          <div className={'player-line'}>{secondPlace.join(' ')}</div>
-          <div className={'player-line'}>{thirdPlace.join(' ')}</div>
-          <div className={'player-line'}>{fourthPlace.join(' ')}</div>
+          <SvgPlayerScoreLine
+            place={PLACE.FIRST}
+            playerScoreCardArray={firstPlace}
+          />
+          <SvgPlayerScoreLine
+            place={PLACE.SECOND}
+            playerScoreCardArray={secondPlace}
+          />
+          <SvgPlayerScoreLine
+            place={PLACE.THIRD}
+            playerScoreCardArray={thirdPlace}
+          />
+          <SvgPlayerScoreLine
+            place={PLACE.FORTH}
+            playerScoreCardArray={fourthPlace}
+          />
+          <foreignObject x="100" y="600" height="100" width="500">
+            <div xmlns="http://www.w3.org/1999/xhtml" className={'player-line'}>
+              {firstPlace.join(' ')}
+            </div>
+            <div xmlns="http://www.w3.org/1999/xhtml" className={'player-line'}>
+              {secondPlace.join(' ')}
+            </div>
+            <div xmlns="http://www.w3.org/1999/xhtml" className={'player-line'}>
+              {thirdPlace.join(' ')}
+            </div>
+            <div xmlns="http://www.w3.org/1999/xhtml" className={'player-line'}>
+              {fourthPlace.join(' ')}
+            </div>
+          </foreignObject>
         </SvgGameResultsContainer>
       </div>
     </div>
--- cli/gameresults/svg_gameresults_playerscoreline.tsx
+++ cli/gameresults/svg_gameresults_playerscoreline.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
 
+import PlayerScoreStats from './svg_gameresults_playerscorestats';
+
+export const enum PLACE {
+  FIRST = 0,
+  SECOND,
+  THIRD,
+  FORTH,
+}
+
+export const SCORELINE_ATTRIBUTES = {
+  height: 75,
+  width: 400,
+  rx: 5,
+  ry: 5,
+};
+
 export default function SvgPlayerScoreLine(props) {
-  return <rect></rect>;
+  const { playerScoreCardArray, place } = props;
+  return (
+    <g
+      transform={`matrix(1,0,0,1,150,${
+        100 + place * (SCORELINE_ATTRIBUTES.height + 25)
+      })`}
+    >
+      <rect {...SCORELINE_ATTRIBUTES}>
+        {<PlayerScoreStats playerScoreCardArray={playerScoreCardArray} />}
+      </rect>
+    </g>
+  );
 }
