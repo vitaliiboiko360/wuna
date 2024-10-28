4503df92591189186db20c97b736b5eb64a1adc6
vitalii boiko <vukelan@gmail.com>
2024-10-28 20:28:25 +0200

    export svg game res dimentions

--- cli/gameresults/svg_gameresults_container.tsx
+++ cli/gameresults/svg_gameresults_container.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const SVG_DIMENSTIONS = {
+export const SVG_GAMERESULTS_DIMENSTIONS = {
   width: 700,
   height: 700,
   viewBox: '0 0 700 700',
 };
 
 export default function SvgGameResultsContainer(props) {
-  return <svg {...SVG_DIMENSTIONS}>{props.children}</svg>;
+  return <svg {...SVG_GAMERESULTS_DIMENSTIONS}>{props.children}</svg>;
 }
