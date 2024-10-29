569e5b9234bfa0734eab39a200c45f16834f62e0
vitalii boiko <vukelan@gmail.com>
2024-10-29 22:02:00 +0200

    add background for game results

---
+++ cli/gameresults/svg_gameresults_background.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { FILTER_BLUR_ID } from './svg_gameresults_shareddefs';
+
+const GAME_RESULTS_BACKGROUND = {
+  filter: `url(#${FILTER_BLUR_ID})`,
+  cx: 0,
+  cy: 0,
+  rx: '7%',
+  ry: '8%',
+  width: '100%',
+  height: '100%',
+  fill: '#d6cae6',
+};
+
+export default function SvgGameResultsBackground(props) {
+  return <rect {...GAME_RESULTS_BACKGROUND}></rect>;
+}
