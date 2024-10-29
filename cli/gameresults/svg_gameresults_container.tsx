53924cd3720d8ae491de2a0e8bcb80ea2ecf5337
vitalii boiko <vukelan@gmail.com>
2024-10-29 22:03:10 +0200

    add extra comps to container game results

--- cli/gameresults/svg_gameresults_container.tsx
+++ cli/gameresults/svg_gameresults_container.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import SvgGameResultsBackground from './svg_gameresults_background';
+import SvgUsersScoreTableWrapper from './svg_gameresults_usersscorewrapper';
+import SvgGameResultsSharedDefs from './svg_gameresults_shareddefs';
+
 export const SVG_GAMERESULTS_DIMENSTIONS = {
-  width: 700,
-  height: 700,
+  width: '75vh',
+  height: '75vh',
   viewBox: '0 0 700 700',
 };
 
 export default function SvgGameResultsContainer(props) {
-  return <svg {...SVG_GAMERESULTS_DIMENSTIONS}>{props.children}</svg>;
+  return (
+    <svg {...SVG_GAMERESULTS_DIMENSTIONS}>
+      {<SvgGameResultsSharedDefs />}
+      {<SvgGameResultsBackground />}
+      {<SvgUsersScoreTableWrapper />}
+      {props.children}
+    </svg>
+  );
 }
