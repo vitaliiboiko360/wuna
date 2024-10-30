1cf791e5fa4bc5dd22c25f346d5c5a8b0fc44dc3
vitalii boiko <vukelan@gmail.com>
2024-10-30 15:02:12 +0200

    add id as AvatarSelector and clenup props

--- cli/svg/svg_userplayeravatar.tsx
+++ cli/svg/svg_userplayeravatar.tsx
@@ -5,31 +5,41 @@ import UserPlayerAvatarLoader from './active_players/userplayeravatarloader';
 export const USERPLACEHOLDER_DIMS = { width: 120, height: 120 };
 
 const UserPlayerAvatar = React.forwardRef((props, ref) => {
-  const transformString = `translate(${props.xPosition},${props.yPosition})`;
+  const { position, xPosition, yPosition, id } = props;
+  const transformString = `translate(${xPosition},${yPosition})`;
   const [isAvatarLoaded, setAvatarLoaded] = useState(false);
-  const clipForRect = `clipForRect${props.position}`;
+  const clipForRect = `clipForRect${position}`;
 
-  return (<>
-    <g transform={transformString}>
-      <defs>
-        <clipPath id={clipForRect}>
-          <rect {...USERPLACEHOLDER_DIMS} rx="7" ry="7" />
-        </clipPath>
-      </defs>
+  return (
+    <>
+      <g id={id} transform={transformString}>
+        <defs>
+          <clipPath id={clipForRect}>
+            <rect {...USERPLACEHOLDER_DIMS} rx="7" ry="7" />
+          </clipPath>
+        </defs>
 
-      <use href={`#${clipForRect}`} strokeWidth="2" stroke="lightgray" />
-      <rect
-        ref={ref}
-        fill={'ghostwhite'}
-        {...USERPLACEHOLDER_DIMS}
-        rx="7" ry="7" strokeWidth="2" stroke="lightgray" />
-      <g clipPath={`url(#${clipForRect})`} >
-        {<UserPlayerAvatarLoader
-          setAvatarLoaded={setAvatarLoaded}
-          position={props.position} />}
+        <use href={`#${clipForRect}`} strokeWidth="2" stroke="lightgray" />
+        <rect
+          ref={ref}
+          fill={'ghostwhite'}
+          {...USERPLACEHOLDER_DIMS}
+          rx="7"
+          ry="7"
+          strokeWidth="2"
+          stroke="lightgray"
+        />
+        <g clipPath={`url(#${clipForRect})`}>
+          {
+            <UserPlayerAvatarLoader
+              setAvatarLoaded={setAvatarLoaded}
+              position={position}
+            />
+          }
+        </g>
       </g>
-    </g>
-  </>);
+    </>
+  );
 });
 
 export default UserPlayerAvatar;
